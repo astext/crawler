a4f8a13835c45995f4e4a04ebbb30b978e03e2cf
Anthony Sallis <astext@gmail.com>
2018-06-17 14:38:11 +1000

    k

---
+++ scrape-drudgeh.js
@@ -0,0 +1,35 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs-extra');
+
+request("http://www.drudgereport.com/", function(error, response, body) {
+  if(error) {
+    console.log("Error: " + error);
+  }
+  console.log("Status code: " + response.statusCode);
+
+  var $ = cheerio.load(body);
+  
+  var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear();
+
+	if(dd<10) {
+		dd = '0'+dd
+	} 
+
+	if(mm<10) {
+		mm = '0'+mm
+	} 
+
+	today = dd +  mm + yyyy;
+	//document.write(today);
+  
+  $('a').each(function( index ) {
+    var title = $(this).text().trim();
+    var link = $(this).attr('href');
+    fs.appendFileSync('drudge_' + today + '.html', '<div class="drudge-scrape"><h3>' + title + '</h3><p><a href='+ link + '>' + link + '</a></p></div>');
+  });
+
+});
